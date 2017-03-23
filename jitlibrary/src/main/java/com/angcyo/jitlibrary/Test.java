7323ed0e78b44c0e6404cd75ae23751749c1a6b6
angcyo <angcyo@126.com>
2017-03-23 15:07:24 +0800

    Initial commit

---
+++ jitlibrary/src/main/java/com/angcyo/jitlibrary/Test.java
@@ -0,0 +1,20 @@
+package com.angcyo.jitlibrary;
+
+import android.util.Log;
+
+/**
+ * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
+ * 项目名称：
+ * 类的描述：
+ * 创建人员：Robi
+ * 创建时间：2017/03/23 15:05
+ * 修改人员：Robi
+ * 修改时间：2017/03/23 15:05
+ * 修改备注：
+ * Version: 1.0.0
+ */
+public class Test {
+    public static void show() {
+        Log.e("angcyo", "测试JitPack");
+    }
+}
